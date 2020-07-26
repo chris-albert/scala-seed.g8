f6dd99b5a6be8395e73bdf7c63773072d40a4605
chris-albert <creasetoph@gmail.com>
2020-07-26 12:40:11 -0700

    Adding zio to dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,14 +1,20 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest  = "org.scalatest" %% "scalatest"   % "3.0.5"
+
+  lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % "3.0.5"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
 
-  lazy val cats       = "org.typelevel" %% "cats-core"   % "1.5.0"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.1.0"
+  lazy val cats       = "org.typelevel"  %% "cats-core"   % "2.0.0"
+  lazy val catsEffect = "org.typelevel"  %% "cats-effect" % "2.1.4"
 
-  lazy val fs2        = "co.fs2" %% "fs2-core" % "1.0.1"
-  lazy val fs2IO      = "co.fs2" %% "fs2-io"   % "1.0.1"
+  lazy val zio        = "dev.zio"        %% "zio"         % "1.0.0-RC21-2"
+  lazy val zioConfig  = "dev.zio"        %% "zio-config"  % "1.0.0-RC25"
+  lazy val zioConfigTypesfe = "dev.zio" %% "zio-config-typesafe" % "1.0.0-RC25"
 
-  lazy val atto = "org.tpolecat" %% "atto-core"    % "0.6.4"
+  lazy val fs2        = "co.fs2"         %% "fs2-core"    % "1.0.1"
+  lazy val fs2IO      = "co.fs2"         %% "fs2-io"      % "1.0.1"
+
+  lazy val atto       = "org.tpolecat"   %% "atto-core"   % "0.6.4"
+
 }
